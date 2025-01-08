2202788eaa33858dbffd2c189fdc25773ae1621c
Bhushan Songire <bhushanbsongire@gmail.com>
2025-01-08 11:03:58 +0530

    Add logging functionality with JSON based protocol for storage and retrieval

---
+++ logger.py
@@ -0,0 +1,66 @@
+import os
+import json
+from threading import Lock
+from datetime import datetime
+from dotenv import load_dotenv
+
+logs = []
+log_count = 1
+log_lock = Lock()
+
+# Load the environment variables
+load_dotenv()
+jsons_folder = os.environ.get('jsons_folder')
+log_file = os.path.join(jsons_folder, 'logs.json')
+
+
+def get_timestamp():
+    return datetime.now().strftime('%Y-%m-%d_%I-%M-%S_%p')
+
+
+def create_log(topic, message, status='Success', client_id=-1):
+    global log_count
+    log = {
+        'id': log_count,
+        'topic': topic,
+        'message': message,
+        'status': status,
+        'client_id': int(client_id),
+        'timestamp': get_timestamp()
+    }
+
+    logs.append(log)
+    log_count += 1
+    save_logs()
+
+
+def get_log(log_id: int) -> dict:
+    for log in logs:
+        if log['id'] == log_id:
+            return log
+    return False
+
+
+def get_log_after(log_id: int) -> list:
+    """Get all logs after the given log_id"""
+    logs_after = []
+    for log in logs:
+        if log['id'] > log_id:
+            logs_after.append(log)
+    return logs_after
+
+
+def get_log_by_topic(topic: str) -> list:
+    """Get all logs with the given topic"""
+    logs_by_topic = []
+    for log in logs:
+        if topic in log['topic']:
+            logs_by_topic.append(log)
+    return logs_by_topic
+
+
+def save_logs():
+    # Acquire the lock before writing to the file
+    with log_lock:
+        with open(log_file, 'w') as f:
+            json.dump(logs, f, indent=4)

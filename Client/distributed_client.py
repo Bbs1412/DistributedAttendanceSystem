37c87f32f020643e561d728fc19cf99fc152a523
Bhushan Songire <bhushanbsongire@gmail.com>
2025-01-09 01:11:49 +0530

    Working scripts with static method implemented

--- Client/distributed_client.py
+++ Client/distributed_client.py
@@ -11,6 +11,7 @@ import time
 import socket
 import threading
 from datetime import datetime
+from networking import receive_message, send_message, handle_send, handle_recv
 
 # ------------------------------------------------------------------------------
 # Global vars :
@@ -21,14 +22,9 @@ SERVER_IP = "localhost"
 SERVER_PORT = 12345
 DEVICE_NAME = socket.gethostname()
 
-
 MODELS_FOLDER = './Models/'
 IMAGES_FOLDER = './Images/'
 JSONS_FOLDER = './Jsons/'
-
-os.makedirs(MODELS_FOLDER, exist_ok=True)
-os.makedirs(IMAGES_FOLDER, exist_ok=True)
-os.makedirs(JSONS_FOLDER, exist_ok=True)
 
 MY_CLIENT_ID = None
 LOGS = []
@@ -38,6 +34,16 @@ file_save_lock = threading.Lock()
 # ------------------------------------------------------------------------------
 # Utility functions:
 # ------------------------------------------------------------------------------
+
+
+def prepare_folder(folder_path: str):
+    """Prepare the folder by deleting all files in it. 
+    Or create it if it doesn't exist."""
+    if os.path.exists(folder_path):
+        for file in os.listdir(folder_path):
+            os.remove(os.path.join(folder_path, file))
+    else:
+        os.makedirs(folder_path, exist_ok=True)
 
 
 def append_log(log: dict):
@@ -59,21 +65,16 @@ def print_header(title=None, pre_lines=1, post_lines=1, header=True, footer=True
 
     print('\n' * pre_lines, end='')
     if header:
-        print(red_color, '-'*80, reset, sep='')
+        print(red_color, '-' * 80, reset, sep='')
     if title:
         print(blue_color, title, reset, sep='')
     if footer:
-        print(red_color, '-'*80, reset, sep='')
+        print(red_color, '-' * 80, reset, sep='')
     print('\n' * post_lines, end='')
 
 # ------------------------------------------------------------------------------
 # Manage the connection with server:
 # ------------------------------------------------------------------------------
-
-
-def out_of_sync():
-    # raise error :  'Server and client are out of sync...'
-    raise Exception("Server and client are out of sync...")
 
 
 def connect_to_server(client_socket):
@@ -88,55 +89,39 @@ def connect_to_server(client_socket):
         client_socket.connect((SERVER_IP, SERVER_PORT))
 
         # R1 - Receive the welcome message from the server:
-        resp = receive_message(client_socket)
-        if resp['topic'] != 'Hi':
-            out_of_sync()
+        handle_recv(*receive_message(client_socket), expected_topic='Hi')
         print('Connected to server successfully')
 
         # S1 - Send the device name to the server:
-        send_message(client_socket, topic="setup", message=DEVICE_NAME)
+        handle_send(*send_message(client_socket,
+                    topic="setup", message=DEVICE_NAME))
         print(f"Sent the device name  \t\t : '{DEVICE_NAME}'")
 
         # R2 - Receive the client-ID from the server:
-        resp = receive_message(client_socket)
-        if resp["topic"] != "Client Id":
-            out_of_sync()
+        resp = handle_recv(
+            *receive_message(client_socket), expected_topic='Client Id')
         MY_CLIENT_ID = resp["message"]
         print(f"Received client-ID \t\t : '{MY_CLIENT_ID}'")
 
-        # S2 - Send 'OK' to the server:
-        send_message(client_socket, topic="OK")
-
         # R3 - Receive the class register form the server:
-        resp = receive_message(client_socket, save_folder=JSONS_FOLDER)
-        if resp["topic"] != "Class Register":
-            out_of_sync()
+        resp = handle_recv(
+            *receive_message(client_socket, save_folder=JSONS_FOLDER),
+            expected_topic='Class Register')
         print(f"Received class register \t : '{resp['data']['filename']}'")
 
-        # S3 - Send 'OK' to the server:
-        send_message(client_socket, topic="ACK")
-
         # R4 - Get the models count and then models:
-        resp = receive_message(client_socket)
-        if resp["topic"] != "Models Count":
-            out_of_sync()
+        resp = handle_recv(
+            *receive_message(client_socket), expected_topic='Models Count')
         models_count = int(resp["message"])
         print(f"Received models count \t\t : '{models_count}'")
-
-        # S5 - Send 'Ready' to the server:
-        send_message(client_socket, topic="Ready")
 
         # R5 - Get the models:
         print(f"Getting {models_count} models from the server :")
         for i in range(models_count):
-            resp = receive_message(client_socket, save_folder=MODELS_FOLDER)
-            if resp["topic"] != "Pickle":
-                out_of_sync()
-
+            resp = handle_recv(
+                *receive_message(client_socket, save_folder=MODELS_FOLDER),
+                expected_topic='Pickle')
             print(f"\t - {resp['data']['filename']}")
-
-            # Ack the model received:
-            send_message(client_socket, topic="Received")
 
         return True
 
@@ -150,51 +135,33 @@ def connect_to_server(client_socket):
 
 def static_load_balancing(client_socket):
     """Static load balancing logic."""
-    try:
-        # R1 - Receive no of images to process from the server:
-        resp = receive_message(client_socket)
-        if resp["topic"] != "Static Images Count":
-            out_of_sync()
 
-        images_count = int(resp["message"])
-        print(f'Received Image Count \t : {images_count}')
-        print(f'Started Processing \t :')
+    # R1 - Receive the images count from the server:
+    resp = handle_recv(*receive_message(client_socket),
+                       expected_topic='Static Images Count')
+    images_count = int(resp["message"])
+    print(f"Total Image count : '{images_count}'")
 
-        # S1 - Send 'Ready' to the server:
-        send_message(client_socket, topic="Ready")
+    # Receive the images and return the responses:
+    for i in range(images_count):
+        # R2 - Receive the image from the server:
+        resp = handle_recv(
+            *receive_message(client_socket, save_folder=IMAGES_FOLDER),
+            expected_topic='Static Image')
 
-        # R2 - Get the images and timestamps from the server:
-        for i in range(images_count):
-            # R2 - Get the image, timestamp:
-            resp = receive_message(client_socket, save_folder='Images')
-            if resp["topic"] != "Static Image":
-                out_of_sync()
+        image_time = resp["message"]
+        image_name = resp["data"]["filename"]
+        print(f"Received image \t : '{image_name}' of [{image_time}]")
 
-            image_name = resp["data"]["filename"]
-            timestamp = resp["message"]
+        # Process the image:
+        print(f"\t\t : Processing... ", end='\r')
+        result = process_image(image_name)
 
-            # S2 - Send 'Received' to the server:
-            send_message(client_socket, topic="Received Image")
-            print(f'\t + {i} - {image_name} \t - Received - ', end='')
+        # Send the result back to the server:
+        handle_send(*send_message(client_socket,
+                    topic="Processed Data", message=json.dumps(result)))
 
-            # Process the image:
-            json_data = process_image(image_name)
-            print('Processed')
-            append_log(json_data)
-
-            # S3 - Send the processed data back to the server:
-            send_message(client_socket, topic="Processed Data",
-                         message=json.dumps(json_data))
-
-        # R3 - Receive 'End' from the server:
-        resp = receive_message(client_socket)
-        if resp["topic"] != "End":
-            out_of_sync()
-
-        return True
-    except Exception as e:
-        return e
-
+    return True
 
 def dynamic_load_balancing(client_socket):
     """Dynamic load balancing logic."""
@@ -218,6 +185,11 @@ def process_image(image_name):
 
 
 def main():
+    # First prepare the necessary folders:
+    prepare_folder(MODELS_FOLDER)
+    prepare_folder(IMAGES_FOLDER)
+    prepare_folder(JSONS_FOLDER)
+
     # Create a client socket:
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.settimeout(10)
@@ -228,23 +200,19 @@ def main():
 
     resp = connect_to_server(client_socket=client_socket)
     if resp == True:
-        print_header(pre_lines=1, post_lines=1, header=False,
-                     title='Connection Initialization phase with server successful.')
+        print_header(
+            pre_lines=1, post_lines=1, header=False,
+            title='Connection Initialization phase with server successful.')
     else:
-        print(resp)
-        return
+        raise Exception(resp)
 
     # Next phase:
     print_header(pre_lines=1, post_lines=1, footer=False,
                  title='Load Balancing Phase with server')
 
     # Get the mode of load balancing:
-    resp = receive_message(client_socket)
-    if resp["topic"] != "Load Balancing":
-        out_of_sync()
-
-    # 'ACK' the server:
-    send_message(client_socket, topic="ACK")
+    resp = handle_recv(*receive_message(client_socket),
+                       expected_topic='Load Balancing')
     load_balancing = resp["message"]
 
     # Load balancing phase:
@@ -257,9 +225,14 @@ def main():
         print("All images processed successfully.")
         print_header(pre_lines=1, post_lines=1, header=False,
                      title='Load Balancing phase successful.')
+
     else:
         print(resp)
-        return
+
+    # Close the client socket:
+    print_header(pre_lines=1, post_lines=1,
+                 title='Client\'s work is done. Closing the client socket 😊')
+    client_socket.close()
 
 
 # ------------------------------------------------------------------------------
--- distributed_server.py
+++ distributed_server.py
@@ -2,11 +2,11 @@ import os
 import json
 import socket
 import threading
-import logger as l
 from datetime import datetime
 from dotenv import load_dotenv
 
-from Client.networking import receive_message, send_message
+import logger as l
+from networking import receive_message, send_message, handle_recv, handle_send
 
 
 # ------------------------------------------------------------------------------
@@ -15,9 +15,11 @@ from Client.networking import receive_message, send_message
 
 
 load_dotenv()
+
 CLASS_REGISTER = os.environ.get('class_register')
 UPLOADS = os.environ.get('upload_folder')
 MODELS = os.environ.get('face_models_folder')
+
 UPLOADED_DATA = os.environ.get('uploaded_data')
 ATTENDANCE_LOG_FILE = os.environ.get('attendance_raw_file')
 ATTENDANCE_REGISTER = os.environ.get('class_attendance')
@@ -36,10 +38,18 @@ clients = {}
 # }
 
 for i in range(NO_OF_CLIENTS):
-    clients[str(i+1)] = None
+    clients[str(i + 1)] = None
 
 lock = threading.Lock()
 responses = []
+
+INFO = '\033[94m[INFO]\033[0m'
+WARN = '\033[93m[WARN]\033[0m'
+ERROR = '\033[91m[ERROR]\033[0m'
+
+# ------------------------------------------------------------------------------
+# Utility functions:
+# ------------------------------------------------------------------------------
 
 
 def append_response(response):
@@ -57,14 +67,14 @@ def get_timestamp():
 # Manage the connections with clients:
 # ------------------------------------------------------------------------------
 
-def handle_error(client_id, error, topic):
-    l.create_log(
-        topic=topic,
-        message=error,
-        status='Error',
-        client_id=client_id
-    )
-    raise Exception(error)
+# def handle_error(client_id, error, topic):
+#     l.create_log(
+#         topic=topic,
+#         message=error,
+#         status='Error',
+#         client_id=client_id
+#     )
+#     raise Exception(error)
 
 
 def handle_client_initialization(client_socket, client_address):
@@ -76,7 +86,7 @@ def handle_client_initialization(client_socket, client_address):
 
         # Dynamically assign the first available client_id:
         for i in range(NO_OF_CLIENTS):
-            cid = str(i+1)
+            cid = str(i + 1)
             if clients[cid] == None:
                 # Assign that client-id
                 client_id = cid
@@ -84,17 +94,18 @@ def handle_client_initialization(client_socket, client_address):
                 clients[cid] = 'hold'
 
         print(
-            f"[INFO] Client {client_id} : Connected Successfully {client_address}")
+            f"{INFO} Client {client_id} : Connected Successfully {client_address}")
 
-        # S1 - Send welcome message
-        send_message(client_socket, topic='Hi')
+        # S1 - Send welcome message to client:
+        handle_send(*send_message(client_socket, topic='Hi'),
+                    log_topic='Connection - Welcome', log_client_id=client_id,
+                    log_success_message='Client welcome message sent successfully.')
 
-        # R1 - Receive client name
-        resp = receive_message(client_socket)
-        if resp['topic'] != 'setup':
-            handle_error(client_id=client_id, topic='Connection',
-                         error="Client didn't send the setup message.")
-
+        # R1 - Receive client name from client:
+        resp = handle_recv(
+            *receive_message(client_socket), expected_topic='setup',
+            log_client_id=client_id, log_topic='Connection - Device Name',
+            log_success_message='Client setup message received successfully.')
         client_name = resp['message']
 
         # Update the shared state:
@@ -104,169 +115,106 @@ def handle_client_initialization(client_socket, client_address):
             "address": client_address
         }
 
-        # S2 - Send client ID
-        send_message(client_socket, topic='Client Id', message=client_id)
+        # S2 - Send client ID assigned to the client:
+        handle_send(
+            *send_message(client_socket, topic='Client Id', message=client_id),
+            log_topic='Connection - Client Info',
+            log_client_id=client_id, log_success_message=client_name)
 
-        # Log the connection:
-        l.create_log(
-            topic='Connection',
-            message=f'Client {client_id} - `{client_name}` connected',
-            status='Success',
-            client_id=client_id
-        )
+        # S3 - Send class file to the client:
+        handle_send(*send_message(
+            client_socket, topic='Class Register', file_path=CLASS_REGISTER),
+            log_client_id=client_id, log_topic='Initialization - Class Register',
+            log_success_message='Class register sent successfully.')
 
-        # R2 - Receive the 'OK' from the client
-        resp = receive_message(client_socket)
-        if resp['topic'] != 'OK':
-            handle_error(client_id=client_id, topic='Connection',
-                         error="Client didn't acknowledge the connection.")
+        # S4 - Send the model count first to client:
+        count = str(len(os.listdir(MODELS)))
+        handle_send(
+            *send_message(client_socket, topic='Models Count', message=count),
+            log_topic='Initialization - Models Count', log_client_id=client_id,
+            log_success_message='Model count sent successfully.')
 
-        # S3 - Send class file
-        send_message(client_socket, topic='Class Register',
-                     file_path=CLASS_REGISTER)
-
-        # R3 - Check for 'ACK' from the client:
-        resp = receive_message(client_socket)
-        if resp['topic'] != 'ACK':
-            handle_error(client_id=client_id, topic='Initialization',
-                         error="Client didn't acknowledge the class register.")
-
-        l.create_log(
-            topic='Initialization',
-            message=f'Sent class register to Client {client_id}',
-            status='Success',
-            client_id=client_id
-        )
-
-        # S4 - Send the model count first and then the models
-        count = len(os.listdir(MODELS))
-        send_message(client_socket, topic='Models Count', message=str(count))
-
-        # R4 - Check if client is 'ready':
-        resp = receive_message(client_socket)
-        if resp['topic'] != 'Ready':
-            handle_error(client_id=client_id, topic='Initialization',
-                         error="Client didn't acknowledge the models.")
-
-        # S5 - Send the models
+        # S5 - Send all the the models to the client:
         for i, file in enumerate(os.listdir(MODELS)):
             file_path = os.path.join(MODELS, file)
-            send_message(client_socket, topic='Pickle', file_path=file_path)
+            handle_send(
+                *send_message(client_socket, topic='Pickle', file_path=file_path),
+                log_topic='Initialization - Models', log_client_id=client_id,
+                log_success_message=f'Model {i} sent successfully.')
 
-            resp = receive_message(client_socket)
-            if resp['topic'] != 'Received':
-                handle_error(client_id=client_id, topic='Initialization',
-                             error=f"Client didn't acknowledge the model {i} - '{file_path}' received.")
-
+        # Create a log for all the models sent successfully:
         l.create_log(
-            topic='Initialization',
-            message=f'Sent all the face models to Client {client_id}',
-            status='Success',
-            client_id=client_id
-        )
+            topic='Initialization - Models', status='Success',
+            client_id=client_id, message=f'Sent all the face models successfully.')
 
         # Mark end of initialization phase
-        msg = f"[INFO] Client {client_id} : Initialization phase completed."
+        msg = f"{INFO} Client {client_id} : Initialization phase completed."
         print(msg)
-        l.create_log(
-            topic='Initialization',
-            message=msg,
-            status='Success',
-            client_id=client_id
-        )
+        l.create_log(topic='Initialization - Complete', message=msg,
+                     status='Success', client_id=client_id)
 
     except Exception as e:
         l.create_log(
-            topic='Connection',
-            message=f'Client {client_id} - `{client_name}` connection error: {e}',
-            status='Error',
-            client_id=client_id
-        )
-        print(
-            f"[ERROR] Client {client_id} Initialization Error \n\t{e}")
+            topic='Connection', status='Error', client_id=client_id,
+            message=f'Client {client_id} - `{client_name}` connection error: {e}')
+        print(f"[ERROR] Client {client_id} Initialization Error \n\t{e}")
 
 
 # ------------------------------------------------------------------------------
 # Load balancing strategies:
 # ------------------------------------------------------------------------------
 
+def handle_error():
+    ...
+
 
 def static_mode_thread(image_list, timestamp_list, client_id):
     """Threaded function to handle the static load balancing."""
+
     client_socket = clients[str(client_id)]['socket']
     client_name = clients[str(client_id)]['name']
 
-    # S0 - Inform client the mode of operation:
-    send_message(client_socket, topic='Load Balancing', message='Static')
-
-    # R0 - Receive the 'ACK' from the client
-    response = receive_message(client_socket)
-    if response['topic'] != 'ACK':
-        handle_error(client_id=client_id, topic='Load Balancing',
-                     error="Client didn't acknowledge the load balancing mode.")
-
     # S1 - Send the image count to the client:
-    send_message(client_socket, topic='Static Images Count',
-                 message=len(image_list))
-
-    # R1 - Receive the 'Ready' from the client
-    response = receive_message(client_socket)
-    if response['topic'] != 'Ready':
-        handle_error(client_id=client_id, topic='Load Balancing',
-                     error="Client didn't acknowledge the image count.")
+    handle_send(*send_message(
+        client_socket, topic='Static Images Count', message=len(image_list)),
+        log_topic='Load Balancing', log_client_id=client_id,
+        log_success_message='Image count sent successfully.')
 
     # Send the images and timestamps to get response:
     for i, (image, timestamp) in enumerate(zip(image_list, timestamp_list)):
-        # S2 - Send the image and timestamp
-        send_message(client_socket, topic='Static Image',
-                     message=timestamp, file_path=image)
+        # S2 - Send the timestamp:
+        handle_send(*send_message(
+            client_socket, topic='Static Image',
+            message=timestamp, file_path=image),
+            log_topic='Load Balancing', log_client_id=client_id,
+            log_success_message=f'Image {i} - [{timestamp}] sent successfully.')
 
-        # R2 - Check for ACK from the client:
-        response = receive_message(client_socket)
-        if response['topic'] != 'Received Image':
-            handle_error(client_id=client_id, topic='Load Balancing',
-                         error=f"Client didn't acknowledge the image {i} - '{image}' received.")
+        print(f"{INFO} Client {client_id} : Image {i} - [{timestamp}] sent.")
 
-        l.create_log(
-            topic='Load Balancing',
-            message=f'Sent image {i} to Client {client_id}',
-            status='Info',
-            client_id=client_id
-        )
+        # R1 - Receive the processed data from the client:
+        resp = handle_recv(
+            *receive_message(client_socket), expected_topic='Processed Data',
+            log_topic='Load Balancing', log_client_id=client_id,
+            log_success_message=f'Image {i} - [{timestamp}] processed successfully.')
 
-        # R3 - Receive the response from the client
-        response = receive_message(client_socket)
-        if response['topic'] != 'Processed Data':
-            handle_error(client_id=client_id, topic='Load Balancing',
-                         error=f"Client didn't process the image {i} - '{image}'.")
-
-        # Save the response
-        processed_data = json.loads(response['message'])
+        # Save the response:
+        processed_data = json.loads(resp['message'])
         append_response(processed_data)
 
-        l.create_log(
-            topic='Load Balancing',
-            message=f'Processing image {i} from Client {client_id} completed',
-            status='Success',
-            client_id=client_id
-        )
-
-    # S3 - Send the 'End' message to the client
-    send_message(client_socket, topic='End')
-
-    print(f"[INFO] Client {client_id} :  All Processing completed.")
+    print(f"{INFO} Client {client_id} :  All Image Processing completed.")
 
 
 def static_mode(image_files, timestamps, frames_count):
     """Static load balancing strategy."""
-    print("[INFO] Static mode selected. Starting static load balancing...")
 
+    print(f"{INFO} Static mode selected. Starting static load balancing...")
     per_client = frames_count // NO_OF_CLIENTS
 
-    divided_data = [{
-        "images": image_files[i*per_client: i*per_client + per_client],
-        "timestamps": timestamps[i*per_client: i*per_client + per_client],
-    } for i in range(NO_OF_CLIENTS)]
+    divided_data = [
+        {
+            "images": image_files[i * per_client: i * per_client + per_client],
+            "timestamps": timestamps[i * per_client: i * per_client + per_client]
+        } for i in range(NO_OF_CLIENTS)]
 
     # Code here for the part to split the images to process them in parallel
     threads = []
@@ -286,7 +234,7 @@ def static_mode(image_files, timestamps, frames_count):
     for thread in threads:
         thread.join()
 
-    print("[INFO] Static load balancing completed.")
+    print(f"{INFO} Static load balancing completed.")
 
 
 def dynamic_mode(client_sockets):
@@ -299,13 +247,14 @@ def dynamic_mode(client_sockets):
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.settimeout(20)
+    server_socket.settimeout(TIMEOUT)
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
-    print(f"[INFO] Server started at `{HOST}:{PORT}`")
+    print(f"{INFO} Server started at `{HOST}:{PORT}`")
 
-    l.create_log('Server', f"Server started at `{HOST}:{PORT}`", 'Success')
-    server_socket.settimeout(TIMEOUT)
+    l.create_log(
+        topic='Server', status='Info', client_id=-1,
+        message=f"Server started at `{HOST}:{PORT}` for timeout = {TIMEOUT} seconds.")
 
     client_sockets = []
     client_threads = []
@@ -326,8 +275,7 @@ def main():
     for client_init_thread in client_threads:
         client_init_thread.join()
 
-    print(
-        f"[INFO] All {NO_OF_CLIENTS} clients connected. Starting load balancing...")
+    print(f"{INFO} All {NO_OF_CLIENTS} clients connected. Starting load balancing...")
 
     # Read the uploaded_data json file:
     with open(UPLOADED_DATA, 'r') as f:
@@ -338,32 +286,46 @@ def main():
     frames_count = data['frame_count']
     processing_mode = data['processing_mode']
 
+    # Log the mode of operation:
+    l.create_log(client_id=-1, topic='Load Balancing - Mode',
+                 message=processing_mode, status='Info')
+
+    # Send: Inform clients the mode of operation:
+    for i in range(NO_OF_CLIENTS):
+        client_id = str(i + 1)
+        client_socket = clients[client_id]['socket']
+
+        handle_send(*send_message(
+            client_socket, topic='Load Balancing', message=processing_mode),
+            log_topic='Load Balancing - Mode', log_client_id=client_id,
+            log_success_message='Load balancing mode sent successfully.')
+
     # Start the load balancing strategy
     if processing_mode.lower() == 'static':
-        l.create_log(
-            topic='Load Balancing',
-            message='Static mode selected. Starting static load balancing...',
-            status='Info'
-        )
         static_mode(image_files, timestamps, frames_count)
 
     elif processing_mode.lower() == 'dynamic':
-        l.create_log(
-            topic='Load Balancing',
-            message='Dynamic mode selected. Starting dynamic load balancing...',
-            status='Info'
-        )
         dynamic_mode(image_files, timestamps, frames_count)
 
     else:
-        handle_error(client_id=-1, topic='Load Balancing',
-                     error="Invalid mode selected. Exiting...")
+        msg = f"[ERROR] Invalid processing mode: {processing_mode}."
+        msg += "Please select either 'static' or 'dynamic'."
+        l.create_log(topic='Load Balancing - Mode',
+                     status='Error', client_id=-1, message=msg)
+        raise ValueError(msg)
+
+    # Compile the results here only or hand it over to the main flask server
+    ...
 
     # Close all client sockets and the server socket
+    l.create_log(topic='Server', status='Info', client_id=-1,
+                 message="Shutting down the server and all client connections.")
+
     for client_socket in client_sockets:
         client_socket.close()
+    print(f"{INFO} Clients disconnected.")
     server_socket.close()
-    print("[INFO] Server shut down.")
+    print(f"{INFO} Server shut down.")
 
 
 # ------------------------------------------------------------------------------
